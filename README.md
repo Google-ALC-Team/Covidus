15b9cc79c148f0ba70ea4c7e7c683f1d6e53abd6
Chidiebere Uchechukwu <uchechidi9@gmail.com>
2020-10-16 09:04:10 +0100

    first commit

--- README.md
+++ README.md
@@ -15,3 +15,22 @@ An information platform that helps international travellers navigate their way w
 *Users should be able to find travel requirement for each African country (such as required number of days of Covid test,Airport Covid19 protocols, amount charged for Covid19 test in each country, link to the country website for more details about Covid19 protocols with respect to travel and any additional infornation considered relevant. )on selection of their prefered country.
 *Users should be able to donate via gofund me account for each country to help those travelling to their country of origin but cannot afford to pay for the test before entrance.
 *Users who are travelling to their country of origin  should be able to contact us to assist with such payment with reasons and proof (such as international passport and contact details and reason they are unable to make such payment).
+
+
+# install dependencies
+
+    $ npm install
+
+# install nodemon globally on your localserver
+
+    $ npm i nodemon -g
+
+# to start the server 
+
+    nodemon
+    
+    
+
+
+
+        
--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const app = express();
 const path = require('path')
-const exhbs = require('express-handlebars')
-const configure = require('./servers/configure')
 const mongoose = require('mongoose')
-
 const dotenv = require('dotenv')
-
-
 dotenv.config({ path:'./config.env'});
-// var  MONGODB_URI = "mongodb+srv://uchechidi:Easytech@cluster0shoppingcart-kcinc.mongodb.net/cluster0shoppingcart?retryWrites=true&w=majority";
-//var MONGODB_URI = "mongodb://uchechidi:Easytech@cluster0shoppingcart-shard-00-00-kcinc.mongodb.net:27017,cluster0shoppingcart-shard-00-01-kcinc.mongodb.net:27017,cluster0shoppingcart-shard-00-02-kcinc.mongodb.net:27017/shopping?ssl=true&replicaSet=Cluster0shoppingcart-shard-0&authSource=admin&retryWrites=true&w=majority"
+
+
+// MONGO DB ENVIRONMENTAL VARIABLE
 const DB = process.env.MONGODB_URI
+
+// DATABASE CONNECTION WITH MONGOOSE
 mongoose.connect(DB,{useNewUrlParser:true,useUnifiedTopology:true});
 mongoose.connection.on('error', console.error.bind(console, 'Database connection Error')); /// handling the error
 mongoose.connection.once('open', function(){
@@ -20,12 +18,14 @@ mongoose.connection.once('open', function(){
 
 
 app.get('/',function(req,res){
-    res.json('')
+    res.json({
+        name:"name"
+    })
 })
 
-
+// APP LISTENING AT PORT:4000
 const port = app.get('port')
-app.listen(process.env.PORT || 2029, function () {
+app.listen(process.env.PORT || 4000, function () {
     console.log(`app listening at port: `)
 })
 
