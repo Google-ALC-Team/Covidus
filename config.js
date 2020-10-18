a1559d8425c792186403e748081856cb5953d170
Chidiebere Uchechukwu <uchechidi9@gmail.com>
2020-10-18 14:59:58 +0100

    login and signup funtionality both with google and facebook auth ..no comments on the code

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ module.exports = {
     mailer:{
         service:'Gmail',
         auth: {
-            user: 'uchechidi5@gmail.com',
-            pass:"081023533"
+            user: '',
+            pass:""
         }
     },
     dbconnection:'mongodb://127.0.0.1:27017/code4share',
