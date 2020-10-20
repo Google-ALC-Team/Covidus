b32821b74af261932bd0b6c5298383d71a300593
dimejismile <42398304+dimejismile@users.noreply.github.com>
2020-10-20 08:14:24 +0100

    New Commit to DonatePage

--- Js/donation_script.js
+++ Js/donation_script.js
@@ -1,8 +1,9 @@
-//When a user select a country form the drop-down list, the button should appear
 
-document.getElementById("button").addEventListener(click, showResult);
-  
-  function showResult() {
-    document.getElementsByClassName("search-result").style.display = "block";
-  }
-  alert("I suck at JS");
+let getDonateLinkButton = document.getElementById("button");
+let searchResult = document.getElementsByClassName("search-result");
+
+getDonateLinkButton.addEventListener("click", showSearchResult);
+
+function showSearchResult() {
+  searchResult.style.display = "block";
+}
